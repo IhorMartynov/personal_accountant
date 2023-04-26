ddcfc833d61a0669c376727a9b97434378d9f5a4
Ihor Martynov <ihor.martynov@ukad-group.com>
2023-04-26 20:09:40 +0300

    Added MongoDB client classes. Added UserSettingsRepository.

--- PersonalAccountant.Web/PersonalAccountant.Db/Models/User.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PersonalAccountant.Db.Models;
-
-public sealed class User
-{
-	public Guid Id { get; set; }
-	public string Name { get; set; }
-	public string Email { get; set; }
-}
---
+++ PersonalAccountant.Web/PersonalAccountant.Db/Models/UserSettings.cs
@@ -0,0 +1,15 @@
+﻿namespace PersonalAccountant.Db.Models;
+
+public sealed record UserSettings(
+	string Id,
+	string Name,
+	string Email,
+	GasAccountSettings GasAccount,
+	ElectricityAccountSettings ElectricityAccount,
+	WaterAccountSettings WaterAccount);
+
+public sealed record GasAccountSettings(string AccountNumber, string Login, string EncryptedPassword);
+
+public sealed record ElectricityAccountSettings(string AccountNumber, string Login, string EncryptedPassword);
+
+public sealed record WaterAccountSettings(string AccountNumber, string Login, string EncryptedPassword);
